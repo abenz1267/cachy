b4c255d3f466e79493b4748a1430d916138f746b
Andrej Benz <hello@andrejbenz.com>
2018-12-07 19:29:48 +0100

    initial

---
+++ cachy.go
@@ -0,0 +1,134 @@
+package cachy
+
+import (
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var wDir string
+
+// Cachy represents the template cache
+type Cachy struct {
+	funcs           template.FuncMap
+	templates       map[string]*template.Template
+	multiTmpls      map[string]*template.Template
+	stringTemplates map[string]string
+}
+
+// Init processes all templates and returns a populated Cachy struct
+func Init(tmplExt string, watch bool, funcs template.FuncMap, folders ...string) (c Cachy, err error) {
+	c.templates = make(map[string]*template.Template)
+	c.multiTmpls = make(map[string]*template.Template)
+	c.stringTemplates = make(map[string]string)
+	c.funcs = funcs
+
+	wDir, err = os.Getwd()
+	if err != nil {
+		return
+	}
+
+	if len(folders) == 0 {
+		folders, err = walkDir(wDir)
+		if err != nil {
+			return
+		}
+	}
+
+	err = load(tmplExt, watch, &c, funcs, folders)
+
+	return
+}
+
+// Execute executes the given template(s).
+func (c Cachy) Execute(w io.Writer, data interface{}, files ...string) (err error) {
+	if len(files) == 1 {
+		c.templates[files[0]].Execute(w, data)
+		return
+	}
+
+	templates := strings.Join(files, ",")
+
+	if val, exists := c.multiTmpls[templates]; exists {
+		return val.Execute(w, data)
+	}
+
+	tmpl := template.New("tmpl").Funcs(c.funcs)
+
+	for _, v := range files {
+		if val, exists := c.stringTemplates[v]; exists {
+			tmpl.Parse(val)
+		} else {
+			log.Fatalf("There is no template '%s'", v)
+		}
+	}
+
+	c.multiTmpls[templates] = tmpl
+
+	tmpl.Execute(w, data)
+
+	return
+}
+
+func load(tmplExt string, watch bool, c *Cachy, funcs template.FuncMap, folders []string) (err error) {
+	dirs := make(map[string][]os.FileInfo)
+	for _, v := range folders {
+		files, err := ioutil.ReadDir(filepath.Join(wDir, v))
+		if err != nil {
+			return err
+		}
+
+		dirs[v] = files
+	}
+
+	for k, v := range dirs {
+		for _, file := range v {
+			if !file.IsDir() && strings.HasSuffix(file.Name(), tmplExt) {
+				if err := cache(c, funcs, k, file.Name(), tmplExt); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func cache(c *Cachy, funcs template.FuncMap, path, file, tmplExt string) (err error) {
+	// parse template and cache it
+	tmpl, err := template.New(file).Funcs(funcs).ParseFiles(filepath.Join(wDir, path, file))
+	if err != nil {
+		return err
+	}
+
+	clearPath := filepath.Join(strings.TrimPrefix(path, "/"), strings.TrimSuffix(file, tmplExt))
+
+	c.templates[clearPath] = tmpl
+
+	// parse string representation of template and cache it
+	b, err := ioutil.ReadFile(filepath.Join(wDir, path, file))
+	if err != nil {
+		return err
+	}
+
+	c.stringTemplates[clearPath] = string(b)
+
+	return
+}
+
+func walkDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			if !strings.Contains(info.Name(), ".") {
+				files = append(files, strings.TrimPrefix(path, root))
+			}
+		}
+		return nil
+	})
+	return files, err
+}
---
+++ cachy_test.go
@@ -0,0 +1,57 @@
+package cachy
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	c, err := Init(".html", false, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var b bytes.Buffer
+	err = c.Execute(&b, nil, "test_templates/base", "test_templates/index")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b.Reset()
+	err = c.Execute(&b, nil, "test_templates/base", "test_templates/index")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b.Reset()
+	err = c.Execute(&b, nil, "test_templates/base")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func BenchmarkExecuteSingleTemplate(b *testing.B) {
+	c, err := Init(".html", false, nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	var w bytes.Buffer
+	for n := 0; n < b.N; n++ {
+		c.Execute(&w, nil, "test_templates/base")
+		w.Reset()
+	}
+}
+
+func BenchmarkExecuteDualTemplate(b *testing.B) {
+	c, err := Init(".html", false, nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	var w bytes.Buffer
+	for n := 0; n < b.N; n++ {
+		c.Execute(&w, nil, "test_templates/base", "test_templates/index")
+		w.Reset()
+	}
+}
